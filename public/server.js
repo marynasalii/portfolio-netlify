d00aebf5d8ec06f7ed9cbe6f27505bfb07d518fb
saliimaryna <lovemarinko@ukr.net>
2019-10-30 16:45:12 -0400

    Lambda added

--- public/server.js
+++ public/server.js
@@ -1,26 +1,29 @@
-const express = require('express');
-const serverless = require('serverless-http');
+// const express = require('express');
+// const serverless = require('serverless-http');
 
-const app = express();
-const router = express.Router();
+// const app = express();
+// const router = express.Router();
 
-//const port = 5000;
+// //const port = 5000;
 
-//app.use(express.static(__dirname + '/'));
+// //app.use(express.static(__dirname + '/'));
 
-router.get('/', (req, res) => {
-    res.sendFile(__dirname + 'Users/maii/Desktop/portfolio-netlify/index.html');
-});
+// router.get('/', (req, res) => {
+//     res.sendFile(__dirname + 'Users/maii/Desktop/portfolio-netlify/index.html');
+// });
 
-router.get('/download', (req, res) => {
-    var file = __dirname + 'Users/maii/Desktop/portfolio-netlify/public/Maryna Salii Resume.pdf';
-    res.download(file);
-});
+// router.get('/download', (req, res) => {
+//     var file = __dirname + 'Users/maii/Desktop/portfolio-netlify/public/Maryna Salii Resume.pdf';
+//     res.download(file);
+// });
 
-//app.listen(process.env.PORT || port);
+// //app.listen(process.env.PORT || port);
 
-app.use('/.netlify/functions/server', router);
+// app.use('/.netlify/functions/server', router);
 
-module.exports = app;
-module.exports.handler = serverless(app);
+// module.exports = app;
+// module.exports.handler = serverless(app);
+
+
+
 
